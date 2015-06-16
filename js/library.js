6e7380211845b984df576246441da948a6b5f989
Sergey <cherrer@mail.ru>
2015-06-16 16:47:55 +0400

    Added InputForm (div) in main html Created FormView to interact with InputForm and BookModel

--- js/library.js
+++ js/library.js
@@ -2,7 +2,8 @@ window.App = {};
 
 window.onload = function() {
   App.bookModel = new App.BookModel();
-  App.bookView = new App.BookView({model: App.bookModel});
+  App.bookView = new App.BookView( {model: App.bookModel} );
+  App.formView = new App.FormView( {model: App.bookModel} );
 }
 
 App.BookModel = Backbone.Model.extend({
@@ -44,6 +45,46 @@ App.BookView = Backbone.View.extend({
 
   render: function() {
     this.$el.html ( this.takeInLine() );
+    return this;
+  }
+});
+
+App.FormView = Backbone.View.extend({
+  el: '#inputForm',
+
+  events: {
+    "keyup .autor": "setAutor",
+    "keyup .title": "setTitle",
+    "keyup .year": "setPYear"
+  },
+
+  initialize: function() {
+    this.model.on( 'invalid', function(model, error, options){
+      this.$el.find(options.name+'Error').text( options.validationError );
+      console.log( options );
+    });
+  },
+
+  setAutor: function() {
+    this.clear('.autorError');
+    this.model.set( 'autor', this.$('.autor').val(), {validate:true, name:'.autor'});
+  },
+
+  setTitle: function() {
+    this.clear('.titleError');
+    this.model.set( 'title', this.$('.title').val(), {validate:true, name:'.title'});
+  },
+
+  setYear: function() {
+    this.clear('.yearError');
+    this.model.set( 'year', '' + this.$('.year').val(), {validate:true, name:'.year'});
+  },
+
+  clear: function(elem) {
+    this.$el.find(elem).text( '' );
+  },
+
+  render: function() {
     return this;
   }
 });
