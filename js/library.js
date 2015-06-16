61715617c1c10a1340fdb5cab82188bf02cefefc
Sergey <cherrer@mail.ru>
2015-06-16 14:54:46 +0400

    Created model App.Book with primitive validation

--- js/library.js
+++ js/library.js
@@ -1,5 +1,29 @@
 window.App = {};
 
 window.onload = function() {
-  
+
 }
+
+App.BookModel = Backbone.Model.extend({
+  defaults: {
+    autor: '',
+    title: '',
+    year: ''
+  },
+
+  validate: function( attrs ) {
+    console.log(attrs);
+ 
+    if ( ! attrs.autor ) {
+      return 'Attribute AUTOR is not filled';
+    }
+
+    if ( ! attrs.title ) {
+      return 'Attribute TITLE is not filled';
+    }
+
+    if ( ! attrs.year ) {
+      return 'Attribute YEAR is not filled';
+    } 
+  }
+});
